c47e412a5580362753a12935ca6ed33cc7b97772
Ruan Augusto Alves <ruanaugust.alves@gmail.com>
2025-12-19 13:24:29 -0300

    Update localizacoes.py

--- backend/routes/localizacoes.py
+++ backend/routes/localizacoes.py
@@ -1,16 +1,17 @@
-from flask import Blueprint, request, jsonify
-from app import db
+from flask import Blueprint, request, jsonify, current_app
 from models import Localizacao
 
 localizacoes_bp = Blueprint('localizacoes', __name__, url_prefix='/api/localizacoes')
 
 @localizacoes_bp.route('', methods=['GET'])
 def listar():
+    from app import db
     locs = db.session.query(Localizacao).all()
     return jsonify([{'id': l.id, 'nome': l.nome, 'lat': l.latitude, 'lng': l.longitude} for l in locs])
 
 @localizacoes_bp.route('', methods=['POST'])
 def criar():
+    from app import db
     data = request.get_json()
     nova_loc = Localizacao(nome=data['nome'], latitude=data['latitude'], longitude=data['longitude'])
     db.session.add(nova_loc)
@@ -19,6 +20,7 @@ def criar():
 
 @localizacoes_bp.route('/<int:id>', methods=['PUT'])
 def atualizar(id):
+    from app import db
     loc = db.session.query(Localizacao).get(id)
     data = request.get_json()
     loc.nome = data.get('nome', loc.nome)
@@ -27,6 +29,7 @@ def atualizar(id):
 
 @localizacoes_bp.route('/<int:id>', methods=['DELETE'])
 def deletar(id):
+    from app import db
     db.session.query(Localizacao).filter_by(id=id).delete()
     db.session.commit()
     return jsonify({'sucesso': True})
